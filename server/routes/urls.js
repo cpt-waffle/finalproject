7ed2f3dc634a44647b118f6f94b91142f881c2f1
vasiliy-klimkin <vasily.klimkin@gmail.com>
2023-02-02 12:11:18 -0500

    add urls database

--- server/routes/urls.js
+++ server/routes/urls.js
@@ -2,8 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+
+// fake database
+
+const urlsDatabase = {
+  BxnV6: 'www.reddit.com',
+  Cr5f5: 'www.github.com',
+}
+
 router.get('/', (req, res ) => {
-  res.json({greetings: 'hello'});
+  res.json({urlsDatabase});
 });
 
 module.exports = router;
